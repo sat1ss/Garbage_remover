944bf33f646d33415fb5140e75081f179fc47f82
sat1ss <91979391+sat1ss@users.noreply.github.com>
2022-11-12 12:42:00 +0100

    Update Garbage_remover.py
    correcting misspelled words

--- Garbage_remover.py
+++ Garbage_remover.py
@@ -13,11 +13,11 @@ for folder in folders:
         try:
             if os.path.isfile(file_path) or os.path.islink(file_path):
                 os.unlink(file_path)
-                print(f"Deleted fail {file_name}")
+                print(f"Deleted file {file_name}")
 
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
                 print(f"Deleted directory {file_name}")
 
         except Exception as x:
-            print(f"Failed to delete {file_path}. Reson: {x}")
+            print(f"Failed to delete {file_path}. Reason: {x}")
